c62ed8bb370c2d24e66b3c4acbdbb91851f7dc05
Imad Ali Khan Abbasi <imaadabbasi@hotmail.com>
2024-06-01 04:21:52 +0500

    Fix baseUrl
    Fixed the base url so it can run github actions correctly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: process.env.NODE_ENV === 'development' ? '' : '/',
+};
 
 export default nextConfig;
