017195abc3d943527e59583e99c057cb9769438e
imixtron <imad.ali@locai.ai>
2024-06-09 16:46:15 +0500

    quirk

--- src/app/component/article.tsx
+++ src/app/component/article.tsx
@@ -8,7 +8,7 @@ export default function Article() {
 					<strong className="markup--strong markup--mixtapeEmbed-strong">Create your own website &amp; host it for free! (Github)</strong> <br />
 					<em className="markup--em markup--mixtapeEmbed-em">Even though single-page applications (SPAs) are all the rage, Server-Side Rendering (SSR) still has some superpowers up…</em>blog.backinagist.com
 				</a>
-				<a href="https://blog.backinagist.com/create-your-own-website-host-it-for-free-github-7f2811de4896" class="js-mixtapeImage mixtapeImage u-ignoreBlock" data-media-id="2f5cc07c0b9f4c230d225f55993ae67f" data-thumbnail-img-id="1*hueJHYpx25wd-V-H_hhyPA.png" style={{'backgroundImage': 'url(https://cdn-images-1.medium.com/fit/c/320/320/1*hueJHYpx25wd-V-H_hhyPA.png)'}}>
+				<a href="https://blog.backinagist.com/create-your-own-website-host-it-for-free-github-7f2811de4896" className="js-mixtapeImage mixtapeImage u-ignoreBlock" data-media-id="2f5cc07c0b9f4c230d225f55993ae67f" data-thumbnail-img-id="1*hueJHYpx25wd-V-H_hhyPA.png" style={{'backgroundImage': 'url(https://cdn-images-1.medium.com/fit/c/320/320/1*hueJHYpx25wd-V-H_hhyPA.png)'}}>
 				</a>
 			</div>
 		</>
