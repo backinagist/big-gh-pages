a312b060c96298beb402c99bd396a13bdbe917f8
imixtron <imad.ali@locai.ai>
2024-06-09 16:41:49 +0500

    link article on the landing page

---
+++ src/app/component/article.tsx
@@ -0,0 +1,16 @@
+import './article.css';
+
+export default function Article() {
+	return (
+		<>
+			<div data-testid="editorParagraphText" className="graf graf--mixtapeEmbed graf-after--h3 is-selected">
+				<a href="https://medium.com/r/?url=https%3A%2F%2Fblog.backinagist.com%2Fcreate-your-own-website-host-it-for-free-github-7f2811de4896" data-href="https://medium.com/r/?url=https%3A%2F%2Fblog.backinagist.com%2Fcreate-your-own-website-host-it-for-free-github-7f2811de4896" className="markup--anchor markup--mixtapeEmbed-anchor" title="https://blog.backinagist.com/create-your-own-website-host-it-for-free-github-7f2811de4896" data-tooltip="https://medium.com/r/?url=https%3A%2F%2Fblog.backinagist.com%2Fcreate-your-own-website-host-it-for-free-github-7f2811de4896" data-tooltip-position="bottom" data-tooltip-type="link">
+					<strong className="markup--strong markup--mixtapeEmbed-strong">Create your own website &amp; host it for free! (Github)</strong> <br />
+					<em className="markup--em markup--mixtapeEmbed-em">Even though single-page applications (SPAs) are all the rage, Server-Side Rendering (SSR) still has some superpowers up…</em>blog.backinagist.com
+				</a>
+				<a href="https://blog.backinagist.com/create-your-own-website-host-it-for-free-github-7f2811de4896" class="js-mixtapeImage mixtapeImage u-ignoreBlock" data-media-id="2f5cc07c0b9f4c230d225f55993ae67f" data-thumbnail-img-id="1*hueJHYpx25wd-V-H_hhyPA.png" style={{'backgroundImage': 'url(https://cdn-images-1.medium.com/fit/c/320/320/1*hueJHYpx25wd-V-H_hhyPA.png)'}}>
+				</a>
+			</div>
+		</>
+	)
+}
